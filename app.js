125fddc07c265f15eddd503874c0f049d9f3a953
Hussain Tabha <88367061+hussiantabha@users.noreply.github.com>
2021-09-17 12:09:32 +0530

    Error when cash given is less than bill amt

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
 console.log("Linked Mark 10 Cash register app")
 
 let subBtn = document.getElementById("subBtn")
-subBtn.addEventListener("click", ()=>{
-    let billAmt = document.getElementById("billAmt").value
-    let cashGiven = document.getElementById("cashGiven").value
-    console.log(billAmt, cashGiven)    
-    let change = cashGiven - billAmt
-    let errChange = billAmt - cashGiven
-    let notes = [2000,500,200,100,50,20,10,5,1]
-    let dispp = document.querySelectorAll(".dispNotes")
-    // if(billAmt <= 0){
-    //     //console.log("Invalid Amt")
-    //     //document.getElementById("dispErr").innerHTML = "Invalid Amount! "
-    //     alert("Invalid Amount!")
-    // }else{
-    //     console.log("valid amount")
-    //     if(cashGiven > billAmt){
-    //         console.log("Ok!")
-
-    //     }else{
-    //         console.log("Give more cash! ")
-    //         document.getElementById("dispMsg").innerHTML = "Give more cash= " 
-    //     }
-    // }
-    if(cashGiven < billAmt){
-        document.getElementById("dispMsg").innerHTML = "Give more cash"
-    }else{
-        for(let i = 0; i<notes.length;i++){
-            let newChange = Math.trunc(change / notes[i])
-            change = change % notes[i]
-            dispp[i].innerHTML = newChange
-            //console.log(change)
+let bill = document.getElementById("billAmt")
+let cash = document.getElementById("cashGiven")
+let notes = [2000,500,200,100,50,20,10,5,1]
+let dispp = document.querySelectorAll(".dispNotes")
+function calChange(){
+    let billAmt = Number(bill.value);
+    let cashAmt = Number(cash.value);
+    let changeGiven = cashAmt - billAmt;
+    if(billAmt > cashAmt){
+        alert("Invalid Amt")
+    }
+    else{
+        for(let i =0;i<notes.length;i++){
+            let changingChange = Math.trunc(changeGiven/notes[i])
+            changeGiven = changeGiven % notes[i]
+            console.log(notes[i] + " " + changingChange)
+            dispp[i].innerHTML = changingChange
         }
     }
-
-   
-
-})
-
-console.log(3800%2000)
+}
+subBtn.addEventListener("click",calChange)
